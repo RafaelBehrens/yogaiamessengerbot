08da8724730ada56eeb8c5616331b6e26d6a2f28
RafaelBehrens <Rafabehrens@aol.com>
2016-10-03 13:02:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,3 +53,50 @@ function sendMessage(recipientId, message) {
         }
     });
 };
+
+function setGreeting() {
+	request({
+        url: 'https://graph.facebook.com/v2.6/1119887924743051/thread_settings',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            "setting_type": "greeting",
+            "greeting":{
+            	"text": "Hi {{user_first_name}}, I'm a prototype bot by Yoga.ai. I'm currently a bit unsophisticated, but I'll try and let you know the day's upcoming live classes."
+            }
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+setGreeting();
+
+//add get started button for first use
+function setStartButton() {
+	request({
+        url: 'https://graph.facebook.com/v2.6/1119887924743051/thread_settings',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            "setting_type":"call_to_actions",
+  			"thread_state":"new_thread",
+  			"call_to_actions":[{
+      			"payload":"USER_DEFINED_PAYLOAD"
+    		}]
+            }
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+setStartButton();
