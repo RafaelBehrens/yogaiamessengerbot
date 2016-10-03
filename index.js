560383e45ee49092f5c83944d53f0eb99ee2e2e3
RafaelBehrens <Rafabehrens@aol.com>
2016-10-03 14:22:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Hey you!"});
-        } else if (event.postback) {
+  		if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
             sendMessage(event.sender.id, {text: "You just got started"});
         }
