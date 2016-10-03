9e6c1f9edf6b947ae0c301bc63a7633ca26b6a7d
RafaelBehrens <Rafabehrens@aol.com>
2016-10-03 18:30:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function classdatasend(recipientId) {
 	
 	//url for classes JSON
     var url = 'https://yogaia.com/api/lessons?upcoming=1&limit=30';
-	var classes;
+	var classes[];
 	//get JSON, parse it and store it in classes variable
 	request(url, (error, response, body)=> {
   	    if (!error && response.statusCode === 200) {
