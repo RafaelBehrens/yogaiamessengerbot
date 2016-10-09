087dfb27c015e38ceb3b5dfc95c514a9349e6d29
RafaelBehrens <Rafabehrens@aol.com>
2016-10-09 22:04:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ app.post('/webhook', function (req, res) {
             } else {
             	sendMessage(event.sender.id, {text: "I'm sorry, I don't quite understand..."});
             }
+            checkquery.on("end", function (result) {          
+            		client.end(); 
+        		});
         } else if (event.message && event.message.text) {
         	console.log("Message received: " + event.message.text);
         } 
