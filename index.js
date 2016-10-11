27144ef13d6b46aa84eff9f4a60bd9b9bd9c141a
RafaelBehrens <Rafabehrens@aol.com>
2016-10-11 22:06:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function cronJob(){
 	})
 }
 
-var dailyjob = new cron.schedule('* 0 21 * * *', function() {
+var dailyjob = new cron.schedule('* 30 21 * * *', function() {
   	if(cronIsAllowed){
   		cronJob();
   		cronIsAllowed = false;
